d2cdabd02eb79ee4afced857a08444189451066a
Akasha Karam <2024_akasha.karam@iticbcn.cat>
2024-12-09 13:08:56 +0100

    Acabado

--- Practica/plantilla.sql
+++ Practica/plantilla.sql
@@ -7,5 +7,31 @@
 ****************************************************** */
 
 -- Creació de les taules
+--Companyia
+create table Companyia(
+  	nom varchar(40),
+    IATA varchar(6),
+    CODE3 varchar(6),
+    ICAO  varchar(6),
+    pais varchar(40)
+);
 
+create table Avio(
+  	num_serie varchar(30),
+    tipus varchar(10),
+    fabricant varchar(20),
+    any_fabricacio init,
+);
+crete table Aeroport(
+  codi varchar(4),
+  pais varchar(40),
+  ciutat varchar(40),
+  IATA varchar(40),
+  nom varchar(55),
+  any_construccio init
 
+);
+
+create table Mostrador(
+  numero init
+)
